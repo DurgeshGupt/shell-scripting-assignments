78aaecddb5c1f83458a3796c2b6822008d29419d
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-10 13:45:30 +0530

    assignment 5 is there

---
+++ digit.sh
@@ -0,0 +1,25 @@
+read -p "enter a number :" n
+
+if [ $n -eq 1 ]
+then
+    echo " first"
+
+elif [ $n -eq 2 ]
+then 
+     echo "second"
+
+elif [ $n -eq 3 ]
+then
+     echo "third"
+
+elif [ $n  -eq 4 ]
+then 
+      echo "fourth"
+
+elif [ $n -eq 5 ]
+then
+     echo "five"
+
+
+
+fi
---
+++ week.sh
@@ -0,0 +1,29 @@
+read -p "enter a number :" n
+
+if [ $n -eq 1 ]
+then
+    echo "sunday"
+
+elif [ $n -eq 2 ]
+then    
+     echo "monday"
+
+elif [ $n -eq 3 ]
+then
+      echo "tuesday"
+
+elif [ $n -eq 4 ]
+then
+      echo "wednesday"
+elif [ $n -eq 5 ]
+then
+     echo "thursday"
+
+elif [ $n -eq 6 ]
+then
+    echo  "friday"
+
+elif [ $n -eq 7]
+then
+     echo "saterday"
+fi
